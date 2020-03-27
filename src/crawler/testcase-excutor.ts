13c9fae6bc8c90b33f68db98b19e186df911c258
Gua <gua@desgard.com>
2020-03-27 16:52:52 +0800

    实现 testcase 替换逻辑, 待优化注入 Button
    Signed-off-by: Gua <gua@desgard.com>

--- src/crawler/testcase-excutor.ts
+++ src/crawler/testcase-excutor.ts
@@ -3,9 +3,12 @@ export function fetch_new_testcase() {
   let result: string[] = [];
   if (pres && pres.length > 0) {
     const reg: RegExp = /^输入：(.+)输出：.+?$/;
-    for (let i = 0; i < pres.length; ++ i) {
+    for (let i = 0; i < pres.length; i++) {
       const pre: HTMLPreElement = pres[i];
-      const inner_text = pre.innerText.split("\n").reduce((pre, cur) => pre + cur).trim();
+      const inner_text = pre.innerText
+        .split("\n")
+        .reduce((pre, cur) => pre + cur)
+        .trim();
       const m = reg.exec(inner_text);
       if (m) {
         result.push(m[1]);
