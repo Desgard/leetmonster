f7723fbf01ea9a083bc445216fa1af09787aa51f
LinXi <linxi60@gmail.com>
2020-03-29 21:33:46 +0800

    append a new testcase button in the end of the buttons area

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
   entry: {
     backgroundPage: path.join(__dirname, "src/backgroundPage.ts"),
     popup: path.join(__dirname, "src/popup/index.tsx"),
-    content: path.join(__dirname, "src/content-script/main.ts"),
+    content: path.join(__dirname, "src/content-script/main.tsx"),
   },
   output: {
     path: path.join(__dirname, "dist/js"),
