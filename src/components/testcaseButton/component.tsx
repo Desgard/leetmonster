611bdd88e3de9b88f6c5f8e6bf9bbde1491caaaf
Gua <gua@desgard.com>
2020-03-31 13:51:37 +0800

    update style for button and add the counter logic
    Signed-off-by: Gua <gua@desgard.com>

--- src/components/testcaseButton/component.tsx
+++ src/components/testcaseButton/component.tsx
@@ -1,19 +1,23 @@
-import React, { FunctionComponent } from "react";
+import React from "react";
+import { observer } from "mobx-react-lite";
 import "./styles.scss";
 
 type ButtonProps = {
-  handler: () => void;
+  handler: { (): void };
+  tot: number;
 };
 
-export const TestCaseButton: FunctionComponent<ButtonProps> = ({
-  handler,
-}: ButtonProps) => {
+export const TestCaseButton: React.SFC<ButtonProps> = observer(props => {
+  const [counter, setCounter] = React.useState<number>(1);
   return (
     <button
       className="btn btn-info case-button"
-      onClick={(): void => handler()}
+      onClick={() => {
+        props.handler();
+        setCounter((counter % props.tot) + 1);
+      }}
     >
-      <span>用例</span>
+      <span>用例 {counter}</span>
     </button>
   );
-};
+});
--- src/content-script/main.tsx
+++ src/content-script/main.tsx
@@ -1,7 +1,6 @@
 import { fetchNewTestcase } from "@src/crawler/testcase-excutor";
 import { TestCaseButton } from "@src/components/testcaseButton";
 import * as React from "React";
-
 import $ from "jquery";
 import ReactDOM from "react-dom";
 
@@ -10,23 +9,24 @@ const checkInterval = setInterval(() => {
   if (runButtons.length <= 0) return;
   clearInterval(checkInterval);
 
-  // 定位button
+  // 定位 button
   const btnContainer = runButtons
     .parent()
-    .append("<div></div>")
+    .prepend("<div></div>")
     .children()
-    .last()[0];
+    .first()[0];
+
   // 获取数据
   const testcaseResult = fetchNewTestcase();
   let testcaseCount = 0;
-  const testcaseRunner: () => void = () => {
+  const testcaseRunner: { (): void } = () => {
     const current = testcaseResult[testcaseCount];
     testcaseCount += 1;
     testcaseCount %= testcaseResult.length;
     $("textarea[class^='testcase-editor']").val(current);
   };
   ReactDOM.render(
-    <TestCaseButton handler={(): void => testcaseRunner()} />,
+    <TestCaseButton handler={testcaseRunner} tot={testcaseResult.length} />,
     btnContainer,
   );
 }, 1000);
